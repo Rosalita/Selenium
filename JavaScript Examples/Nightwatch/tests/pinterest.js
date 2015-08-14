8ef55371dd632238293fa2b811ac37702c6aa25c
Rosie Hamilton <rhamilton@scottlogic.com>
2015-08-14 16:53:28 +0100

    Added JavaScript example using Nightwatch framework

---
+++ JavaScript Examples/Nightwatch/tests/pinterest.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+
+    'Try to login with no username or password': function( _browser ) {
+    _browser
+      .url('https://www.pinterest.com/')
+      .waitForElementVisible( 'body', 1000 )
+      .click('.emailLogin')
+      .waitForElementVisible('.Button.Module.btn[type=submit]', 5000)
+      .click('.Button.Module.btn[type=submit]')
+      .pause(2000)
+      .verify.visible('input[class=hasError]')
+      .verify
+    },
+
+// To Do
+//  'Try to login with wrong username and password': function( _browser ) {
+//  },
+// 'Try to log in with valid credentials': function( _browser ) {
+//  },
+
+}
